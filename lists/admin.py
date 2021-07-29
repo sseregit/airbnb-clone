2552190d508713a4d9a04bcbc406014bb22cf167
sseregit <39255703+sseregit@users.noreply.github.com>
2021-07-30 01:16:25 +0900

    2021.07.30 AM 01:16

--- lists/admin.py
+++ lists/admin.py
@@ -9,4 +9,12 @@ class ListAdmin(admin.ModelAdmin):
 
     """List Admin Definition"""
 
-    pass
+    list_display = (
+        "name",
+        "user",
+        "count_rooms",
+    )
+
+    filter_horizontal = ("rooms",)
+
+    search_fields = ("name",)
