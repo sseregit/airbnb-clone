2e4be308911660aa81a993158b64c09e6d155061
sseregit <39255703+sseregit@users.noreply.github.com>
2021-08-07 01:28:15 +0900

    2021.08.07 AM 01:28

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,6 @@
 from django.views.generic import ListView
+from django.urls import reverse
+from django.shortcuts import redirect, render
 from . import models
 
 
@@ -11,3 +13,11 @@ class HomeView(ListView):
     ordering = "created"
     paginate_orphans = 5
     context_object_name = "rooms"
+
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        return redirect(reverse("core:home"))
