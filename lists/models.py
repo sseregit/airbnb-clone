7afa67d07066a41cd64027d24a8bcf7999bbbb91
sseregit <39255703+sseregit@users.noreply.github.com>
2021-09-18 23:19:03 +0900

    2021.09.18 PM 11:19

--- lists/models.py
+++ lists/models.py
@@ -9,10 +9,10 @@ class List(core_models.TimeStampedModel):
     """List Model Definition"""
 
     name = models.CharField(max_length=80)
-    user = models.ForeignKey(
-        "users.User", related_name="lists", on_delete=models.CASCADE
+    user = models.OneToOneField(
+        "users.User", related_name="list", on_delete=models.CASCADE
     )
-    rooms = models.ManyToManyField("rooms.Room", related_name="lists", blank=True)
+    rooms = models.ManyToManyField("rooms.Room", related_name="list", blank=True)
 
     def __str__(self):
         return self.name
