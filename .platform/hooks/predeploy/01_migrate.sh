bbf23758860886e40328364f52c24f48b9828ec4
JangYeonSik <sseregit@naver.com>
2021-11-04 14:59:41 +0900

    AWS command Error modify 9

--- .platform/hooks/predeploy/01_migrate.sh
+++ .platform/hooks/predeploy/01_migrate.sh
@@ -4,6 +4,6 @@ source /var/app/venv/*/bin/activate
 cd /var/app/staging
 
 python manage.py migrate
-python manage.py compilemessages
+python manage.py compilemessages 
 python manage.py createsu
 python manage.py collectstatic --noinput
