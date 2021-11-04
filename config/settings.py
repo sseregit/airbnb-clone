e1b41b9e67a9b43ab472c2205a0821bfd817495f
JangYeonSik <sseregit@naver.com>
2021-11-04 17:01:53 +0900

    AWS command Error 20

--- config/settings.py
+++ config/settings.py
@@ -191,6 +191,7 @@ if not DEBUG:
     AWS_S3_OBJECT_PARAMETERS = {"CacheControl":"max-age=86400"}
     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.ap-northeast-2.amazonaws.com"
     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
+    AWS_DEFAULT_ACL = "public-read"
 
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
