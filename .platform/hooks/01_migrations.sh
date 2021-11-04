42f911728096b5f9e3015539460a835cecd258aa
JangYeonSik <sseregit@naver.com>
2021-11-04 13:30:34 +0900

    AWS command Error modify 4

---
+++ .platform/hooks/01_migrations.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /var/app/venv/*/bin/activate
+cd /var/app/staging
+
+python manage.py migrate
+python manage.py compilemessages
+python manage.py createsu
+python manage.py collectstatic --noinput
