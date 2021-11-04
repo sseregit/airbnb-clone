6491cbe23691484c220badfaf8e38926f5e53ab9
JangYeonSik <sseregit@naver.com>
2021-11-04 15:53:27 +0900

    AWS command Error 12

--- config/settings.py
+++ config/settings.py
@@ -148,9 +148,12 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = os.path.join(BASE_DIR,"static")
+if not DEBUG:
+    STATIC_ROOT = ""
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR,"static")
 
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 AUTH_USER_MODEL = "users.User"
 
